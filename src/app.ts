623910a3225c9686cc899ab041f969f15bccf8dd
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-07-09 19:30:17 +0800

    add comment

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,18 @@ import { config } from "dotenv";
 import { httpErrorHandler, errorHandler } from "./middlewares/middlewares";
 import router from "./routes";
 
+// Initialize App
 config();
 const app: Application = express();
 
-app.use("/", router);
+// Middlewares
 app.use(httpErrorHandler);
 app.use(errorHandler);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/", router);
 
 const port: number = Number(process.env.PORT) || 4000;
 app.listen(port, () => console.log(`Server Running on port: ${port}`));
