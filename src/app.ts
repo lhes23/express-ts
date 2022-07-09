fbef374f024fb1384755379e2f86cfef4516bc42
lhes23 <32542078+lhes23@users.noreply.github.com>
2022-07-09 19:35:51 +0800

    added userRoutes

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,21 @@ import express, { Application } from "express";
 import { config } from "dotenv";
 import { httpErrorHandler, errorHandler } from "./middlewares/middlewares";
 import router from "./routes";
+import userRoutes from "./routes/userRoutes";
 
 // Initialize App
 config();
 const app: Application = express();
+
+// Routes
+app.use("/", router);
+app.use("/api/users", userRoutes);
 
 // Middlewares
 app.use(httpErrorHandler);
 app.use(errorHandler);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.use("/", router);
 
 const port: number = Number(process.env.PORT) || 4000;
 app.listen(port, () => console.log(`Server Running on port: ${port}`));
